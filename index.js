f8c2708fdefac85e9c64fb6182557c833e4319d2
Chelsea Fisher <chelseasfisher1@gmail.com>
2022-02-13 19:33:44 -0600

    commit tree e5d619c1ba21b2b32222b0b7ebdb7f8ed4d923e1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+let brunch = new Breakfast("eggs", "juice");
+brunch;
+
+class Lunch {
+    constructor(salad,soup,drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+let midlunch = new Lunch("side salad","broccoli cheddar soup","iced tea");
+midlunch;
+
+class Dinner {
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+
+let evening = new Dinner("balsamic salad","consomme","filet mignon","cheesecake");
+evening;
